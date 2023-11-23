dc84f47a337e8dedb0e18459cbcdf540a160f555
Mradul <mradul9936@gmail.com>
2023-11-23 23:57:46 +0530

    initial commit

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/send-email', async (req, res) => {
     }
     res.render("index",{greet:"Mail or Password is wrong,Try again"})
 });
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server listening on port 3000');
 });
